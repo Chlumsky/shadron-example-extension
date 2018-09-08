91c1cdb496810a2625c1dfe241d3828747699536
Chlumsky <viktor.chlumsky@gmail.com>
2018-09-08 16:09:14 +0200

    Added 64-bit version

--- install-to-shadron.bat
+++ install-to-shadron.bat
@@ -1,8 +1,8 @@
-IF EXIST "Release\shadron-example.dll" (
+IF EXIST "x64\Release\shadron-example.dll" (
     IF NOT EXIST "%APPDATA%\Shadron\extensions\" (
         MD "%APPDATA%\Shadron\extensions\"
     )
-    COPY /Y "Release\shadron-example.dll" "%APPDATA%\Shadron\extensions\shadron-example.dll"
+    COPY /Y "x64\Release\shadron-example.dll" "%APPDATA%\Shadron\extensions\shadron-example.dll"
 ) ELSE (
     ECHO You must build the Release DLL first
 )
